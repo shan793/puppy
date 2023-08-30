45744a26842a74f56850eca24d7a4c23a06c8f2d
shan793 <sgoonatilaka09@gmail.com>
2023-08-30 15:51:25 -0400

    Added dependency from dog into puppy so we can use func in dog package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shan793/puppy
 
 go 1.21.0
+
+require github.com/shan793/dog v0.0.0-20230830194557-a00e8416cfb4 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package puppy
 
+
+import (
+	"github.com/shan793/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+/* we are going to use this function to illustrate how to use
+a function that is in another package in this code as well
+we want to use the function in the dog package 
+*/
+func BigBark() string {
+
+	return dog.WhenGrownUp(Bark())
+
+}
+func BigBarks() string {
+
+	return dog.WhenGrownUp(Barks())
+
 }
